852ee97d20b0e0b9466fd5f77fe893271b58757a
Yuliia7777 <yuliiakorobkova1@gmail.com>
2024-03-10 19:28:47 +0100

    hw07

---
+++ js/task-4.js
@@ -0,0 +1,20 @@
+let form = document.querySelector('.login-form');
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let form = event.target;
+  const email = form.elements.email.value.trim();
+  const password = form.elements.password.value.trim();
+
+  if (email === '' || password === '') {
+    return alert('Please fill in all the fields!');
+  }
+
+  const out = {
+    email: email,
+    password: password,
+  };
+  console.log(out);
+  form.reset();
+}
